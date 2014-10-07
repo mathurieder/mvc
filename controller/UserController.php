2abb0cde664bd17cb20466497e3c2a8b97a08ad8
binzram <binzram@gmail.com>
2014-10-07 11:41:32 +0200

    init commit controller

---
+++ controller/UserController.php
@@ -0,0 +1,10 @@
+<?php
+class UserController{
+	public function index(){
+		$this->create();
+	}
+	
+	public function create(){
+		echo "User erstellen";
+	}
+}
---
+++ lib/Dispatcher.php
@@ -0,0 +1,14 @@
+<?php 
+class Dispatcher{
+	public static function dispatch(){
+		$url = explode('/', trim($_SERVER['REQUEST_URI'], '/'));
+		
+		$controller = !empty($url[0]) ? $url[0] . "Controller" : "DefaultController";
+		$method = !empty($url[1]) ? $url[1] : 'index';
+		
+		require_once ('controller/'.$controller.'.php');
+		$cont = new $controller;
+		$cont->$method();
+		unset($cont);
+	}
+}
