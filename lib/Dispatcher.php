3c023770dfdaea5366ae8b7b725c1e3da15a77b5
Samuel Wicky <samuel.wicky@bbcag.ch>
2014-10-07 15:46:26 +0200

    cleanup

--- lib/Dispatcher.php
+++ lib/Dispatcher.php
@@ -7,7 +7,7 @@ class Dispatcher
 		$url = explode('/', trim($_SERVER['REQUEST_URI'], '/'));
 
 		$controllerName = !empty($url[0]) ? $url[0] . 'Controller' : 'DefaultController';
-		$method = !empty($url[1]) ? $url[1] : 'index';
+		$method         = !empty($url[1]) ? $url[1] : 'index';
 
 		require_once ("controller/$controllerName.php");
 		$controller = new $controllerName();
