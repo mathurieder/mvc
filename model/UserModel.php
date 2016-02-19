cd6f05f1ef9c65342a082b3696e8d266eea6d688
Samuel Wicky <samuel.wicky@bbcag.ch>
2016-02-19 10:51:00 +0100

    Model and ConnecationHandler documented

--- model/UserModel.php
+++ model/UserModel.php
@@ -2,10 +2,32 @@
 
 require_once 'lib/Model.php';
 
+/**
+ * Das UserModel ist zuständig für alle Zugriffe auf die Tabelle "user".
+ *
+ * Die Ausführliche Dokumentation zu Models findest du in der Model Klasse.
+ */
 class UserModel extends Model
 {
+    /**
+     * Diese Variable wird von der Klasse Model verwendet, um generische
+     * Funktionen zur Verfügung zu stellen.
+     */
     protected $tableName = 'user';
 
+    /**
+     * Erstellt einen neuen benutzer mit den gegebenen Werten.
+     *
+     * Das Passwort wird vor dem ausführen des Queries noch mit dem SHA1
+     *  Algorythmus gehashed.
+     *
+     * @param $firstName Wert für die Spalte firstName
+     * @param $lastName Wert für die Spalte lastName
+     * @param $email Wert für die Spalte email
+     * @param $password Wert für die Spalte password
+     *
+     * @throws Exception falls das Ausführen des Statements fehlschlägt
+     */
     public function create($firstName, $lastName, $email, $password)
     {
         $password = sha1($password);
