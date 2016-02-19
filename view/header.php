15615e6c214810bb16079382a0efe919a110cef0
Samuel Wicky <samuel.wicky@bbcag.ch>
2016-02-19 11:22:47 +0100

    Active menu item removed

--- view/header.php
+++ view/header.php
@@ -33,7 +33,7 @@
         </div>
         <div id="navbar" class="collapse navbar-collapse">
           <ul class="nav navbar-nav">
-            <li class="active"><a href="/">Home</a></li>
+            <li><a href="/">Home</a></li>
             <li><a href="/user">Benutzer</a></li>
           </ul>
         </div><!--/.nav-collapse -->
